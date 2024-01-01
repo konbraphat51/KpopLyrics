30b3b3a4d9735c4245cac43849d465dbf6b2d98f
konbraphat51 <konbraphat@gmail.com>
2024-01-02 00:28:26 +0900

    Add: tfidf explanation

--- src/components/IndividualAnalysis.vue
+++ src/components/IndividualAnalysis.vue
@@ -1,6 +1,21 @@
 <template>
 	<div class="IndividualAnalysis">
 		<h3>{{ target }}</h3>
+		<details>
+			<summary>TFIDF (keywords)</summary>
+
+			<details>
+				<summary>What is TFIDF?</summary>
+				<p>
+					TFIDF indicates how important a word is to a document in a corpus.<br />
+					<a
+						href="https://medium.com/nlplanet/two-minutes-nlp-learn-tf-idf-with-easy-examples-7c15957b4cb3"
+					>
+						More information
+					</a>
+				</p>
+			</details>
+		</details>
 	</div>
 </template>
 
@@ -13,3 +28,12 @@ export default {
 	},
 }
 </script>
+
+<style>
+.IndividualAnalysis details {
+	margin-left: 1em;
+}
+.IndividualAnalysis summary {
+	margin-left: -1em;
+}
+</style>
