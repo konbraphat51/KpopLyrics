d8ac855ea224ede74509d8e99474757c55d71574
konbraphat51 <konbraphat@gmail.com>
2024-01-01 21:18:58 +0900

    Add: app vue

---
+++ src/components/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app"></div>
+</template>
+
+<script>
+export default Vue.defineComponent({
+  name: "App",
+  components: {},
+  setup() {
+    //set up i18n
+    const { t } = VueI18n.useI18n();
+    return { t };
+  },
+});
+</script>
+
+<i18n>
+    {
+        "en": {
+            "example": {
+                "explain": "This is how you use i18n"
+            }
+        },
+        "ja": {
+            "example": {
+                "explain": "こうやってi18nを使います"
+            }
+        }
+    }
+</i18n>
