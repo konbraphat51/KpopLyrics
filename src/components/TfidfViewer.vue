57fceb9694d1f1c1cbead7d674abac9d4dd10b78
konbraphat51 <konbraphat@gmail.com>
2024-01-02 01:09:27 +0900

    Add: tfidfViewer
    not yet

---
+++ src/components/TfidfViewer.vue
@@ -0,0 +1,24 @@
+<template>
+	<div :id="tableID"></div>
+</template>
+
+<script>
+export default {
+	name: "TfidfViwer",
+	props: {
+		dataframe: Object,
+	},
+	data() {
+		return {
+			tableID: MakeID(6),
+		}
+	},
+	computed: {
+		dataThis() {
+			if (this.dataframe == undefined) return {}
+
+			return this.dataframe
+		},
+	},
+}
+</script>
