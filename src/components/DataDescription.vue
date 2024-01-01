2aa5eca87d2e1e79187de054be8635a1cac58b1f
konbraphat51 <konbraphat@gmail.com>
2024-01-02 00:04:43 +0900

    Show playlists

--- src/components/DataDescription.vue
+++ src/components/DataDescription.vue
@@ -2,7 +2,7 @@
 	<div id="DataDescription">
 		<h2>Data Description</h2>
 
-		<h3>numbers of each groups</h3>
+		<h3>Numbers of each groups</h3>
 		<table>
 			<tr>
 				<th>group</th>
@@ -13,6 +13,21 @@
 				<td>{{ GetMusicCount(group) }}</td>
 			</tr>
 		</table>
+
+		<h3>Used playlists</h3>
+		<details>
+			<summary>Click to show</summary>
+			<table>
+				<tr>
+					<th>playlist</th>
+					<th>grouping name</th>
+				</tr>
+				<tr v-for="playlist in _playlists" :key="playlist[0]">
+					<td>{{ playlist[0] }}</td>
+					<td>{{ playlist[1] }}</td>
+				</tr>
+			</table>
+		</details>
 	</div>
 </template>
 
@@ -32,12 +47,23 @@ export default {
 		},
 	},
 	computed: {
-		total_music() {
+		totalMusic() {
 			if (this.data == undefined) {
 				return 0
 			} else {
 				return this.data["tracks_total"]
 			}
+		},
+
+		_playlists() {
+			if (this.playlists == undefined) return []
+
+			let output = []
+			for (let i = 1; i < this.playlists.length; i++) {
+				output.push([this.playlists[i][0], this.playlists[i][1]])
+			}
+
+			return output
 		},
 	},
 }
