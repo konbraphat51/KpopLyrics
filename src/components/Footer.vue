6814ff11114ae78c04de3c635e0d1efea0e40905
konbraphat51 <konbraphat@gmail.com>
2024-01-01 21:27:25 +0900

    Vue mentioning

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,5 +2,6 @@
   <footer>
     <p>last updated: 2024/1/1</p>
     <p>C: <a href="https://github.com/konbraphat51">Konbraphat51</a></p>
+    <p>This website made by Vue.js</p>
   </footer>
 </template>
