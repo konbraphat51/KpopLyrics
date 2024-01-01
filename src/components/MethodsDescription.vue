dfcd439c6f13979c5ba2654480a373ec0e773de0
konbraphat51 <konbraphat@gmail.com>
2024-01-02 01:09:12 +0900

    Add: methodsDescription

---
+++ src/components/MethodsDescription.vue
@@ -0,0 +1,13 @@
+<template>
+	<details>
+		<summary>What is TFIDF?</summary>
+		<p>
+			TFIDF indicates how important a word is to a document in a corpus.<br />
+			<a
+				href="https://medium.com/nlplanet/two-minutes-nlp-learn-tf-idf-with-easy-examples-7c15957b4cb3"
+			>
+				More information
+			</a>
+		</p>
+	</details>
+</template>
