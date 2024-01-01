d66effefc4a98197545132579d8f35b205a5c1b1
konbraphat51 <konbraphat@gmail.com>
2024-01-02 01:18:36 +0900

    Add license

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
-  <footer>
-    <p>last updated: 2024/1/1</p>
-    <p>C: <a href="https://github.com/konbraphat51">Konbraphat51</a></p>
-    <p>This website made by Vue.js</p>
-  </footer>
+	<footer>
+		<p>last updated: 2024/1/1</p>
+		<p>C: <a href="https://github.com/konbraphat51">Konbraphat51</a></p>
+		<p>This website made by Vue.js + Tabulator</p>
+	</footer>
 </template>
