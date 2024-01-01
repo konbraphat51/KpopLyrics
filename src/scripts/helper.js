cc057c52decae30a14fdc9483d5e82afc9fef828
konbraphat51 <konbraphat@gmail.com>
2024-01-02 01:38:26 +0900

    Improve: ID start from alphabet
    because of query setter

--- src/scripts/helper.js
+++ src/scripts/helper.js
@@ -1,7 +1,6 @@
 function MakeID(length = 6) {
 	var result = ""
-	var characters =
-		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+	var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 	var charactersLength = characters.length
 	for (var i = 0; i < length; i++) {
 		result += characters.charAt(Math.floor(Math.random() * charactersLength))
