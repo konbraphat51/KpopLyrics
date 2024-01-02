e0b3d7482033d22fadcc669e1c0c3b56943ca0ee
konbraphat51 <konbraphat@gmail.com>
2024-01-02 23:33:48 +0900

    uupdate date

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
 	<footer>
-		<p>last updated: 2024/1/1</p>
+		<p>last updated: 2024/1/2</p>
 		<p>C: <a href="https://github.com/konbraphat51">Konbraphat51</a></p>
 		<p>This website made by Vue.js + Tabulator</p>
 	</footer>
