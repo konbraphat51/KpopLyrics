551751570afbdef63110a747469c943bdb0a1e55
konbraphat51 <konbraphat@gmail.com>
2024-01-04 03:50:49 +0900

    Link to notebook

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,6 +2,11 @@
 	<footer>
 		<p>last updated: 2024/1/2</p>
 		<p>C: <a href="https://github.com/konbraphat51">Konbraphat51</a></p>
+		<p>
+			<a href="https://github.com/konbraphat51/LyricAnalysis">
+				Analysis notebook
+			</a>
+		</p>
 		<p>This website made by Vue.js + Tabulator</p>
 	</footer>
 </template>
