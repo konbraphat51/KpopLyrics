9a2564dda918309c32810032b2fc1b5b717e368a
konbraphat51 <konbraphat@gmail.com>
2024-01-04 03:52:33 +0900

    Style footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-	<footer>
+	<footer id="Footer">
 		<p>last updated: 2024/1/2</p>
 		<p>C: <a href="https://github.com/konbraphat51">Konbraphat51</a></p>
 		<p>
@@ -10,3 +10,13 @@
 		<p>This website made by Vue.js + Tabulator</p>
 	</footer>
 </template>
+
+<style>
+#Footer {
+	text-align: center;
+	background-color: rgb(150, 226, 200);
+	left: 0;
+	bottom: 0;
+	width: 100%;
+}
+</style>
