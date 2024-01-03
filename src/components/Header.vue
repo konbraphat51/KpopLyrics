f37678248c3d58fb7393f8f0748976dab8414a86
konbraphat51 <konbraphat@gmail.com>
2024-01-04 03:52:57 +0900

    Style header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,21 @@
 <template>
-  <header>
-    <h1>Analysis of KPOP Lyrics by Text Mining</h1>
-  </header>
+	<header>
+		<h1>Analysis of KPOP Lyrics by Text Mining</h1>
+	</header>
 </template>
 
 <script>
 export default {
-  name: "Header",
-};
+	name: "Header",
+}
 </script>
+
+<style>
+header {
+	text-align: center;
+	background-color: rgb(150, 226, 200);
+	left: 0;
+	top: 0;
+	width: 100%;
+}
+</style>
